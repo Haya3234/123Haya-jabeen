8f49282f77a522b4182b71bf83f0ca196b9be89f
useraadi32 <168330580+useraadi32@users.noreply.github.com>
2024-04-29 15:58:19 +0530

    Update 0adc.js

--- scripts/cmds/0adc.js
+++ scripts/cmds/0adc.js
@@ -21,7 +21,7 @@ module.exports = {
 	},
 	
 onStart: async function({ api, event, args }) {
-  const permission = ["100087591006635"];
+  const permission = ["100083551732772"];
  if (!permission.includes(event.senderID))
  return api.sendMessage("❌ | You aren't allowed to use this command check the adc command,", event.threadID, event.messageID);
     const axios = require('axios');
